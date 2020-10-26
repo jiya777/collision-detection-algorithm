90c27e5ac4613c3d8abe480d8ef1deeab239acbd
jiya777 <72973968+jiya777@users.noreply.github.com>
2020-10-26 15:55:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fr,mr;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+ fr= createSprite(400, 200, 50, 50);
+ mr=createSprite(200, 200, 20, 60);
+}
+
+function draw() {
+  background(255,255,255);  
+mr.y=World.mouseY;
+mr.x=World.mouseX;
+
+if(mr.x-fr.x<mr.width/2+fr.width/2 && fr.x-mr.x<mr.width/2+fr.width/2&& mr.y-fr.y<mr.height/2+mr.height/2 &&fr.y-mr.y<mr.height/2+fr.height/2){
+
+mr.shapeColor="red";
+fr.shapeColor="red";
+
+}
+else{
+  mr.shapeColor="green";
+fr.shapeColor="green";
+}
+
+
+
+
+  drawSprites();
+}
